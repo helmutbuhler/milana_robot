2e85a6a75883294bcc9f4686c5f50b70fe9fe114
Helmut Buhler <buhler@8gadgetpack.net>
2024-07-10 17:44:38 +0200

    Fix ODrive data corruption by setting ack to true

--- common/odrive/ODrive.cpp
+++ common/odrive/ODrive.cpp
@@ -391,6 +391,11 @@ void ODrive::endpoint_request(int endpoint_id,
 		return;
 	endpoint_request_counter++;
 
+	// ODrive somehow sometimes sends corrupt data when the baudrate is about 921600 and ack is set to false,
+	// even if we don't read the response.
+	// Setting it to true fixes this completely.
+	ack = true;
+
 	if (ack)
 		endpoint_id |= 0x8000;
 
